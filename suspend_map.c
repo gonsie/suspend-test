c917822f8b275764dae68e87b658f5e4bab95368
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-09-03 16:12:20 -0400

    suspend_map needs updating for round-robin

--- suspend_map.c
+++ suspend_map.c
@@ -6,7 +6,7 @@
 //Given an LP's GID (global ID)
 //return the PE (aka node, MPI Rank)
 tw_peid suspend_map(tw_lpid gid){
-  return (tw_peid) gid / g_tw_nlp;
+  return (tw_peid) gid % tw_nnodes();
 }
 
 // Multiple LP Types mapping function
