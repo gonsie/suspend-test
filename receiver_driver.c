db12aaa8adfa10fbce81b89faf0e4d05eb0e9cd3
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-09-03 14:40:32 -0400

    receiver LP functionality

--- receiver_driver.c
+++ receiver_driver.c
@@ -5,14 +5,6 @@
 #include "ross.h"
 #include "suspend.h"
 
-//Helper Functions
-void SWAP (double *a, double *b) {
-  double tmp = *a;
-  *a = *b;
-  *b = tmp;
-}
-
-
 //Init function
 // - called once for each LP
 // ! LP can only send messages to itself during init !
@@ -20,90 +12,60 @@ void receiver_init (state *s, tw_lp *lp) {
   int self = lp->gid;
 
   // init state data
-  s->rcvd_count_H = 0;
-  s->rcvd_count_G = 0;
-  s->value = -1;
-
-  // Init message to myself
-  tw_event *e = tw_event_new(self, 1, lp);
-  message *msg = tw_event_data(e);
-  msg->type = HELLO;
-  msg->contents = tw_rand_unif(lp->rng);
-  msg->sender = self;
-  tw_event_send(e);
+  s->count = 0;
+  s->forward_event_count = 0;
+  s->reverse_event_count = 0;
+  s->net_event_count = 0;
 }
 
 //Forward event handler
 void receiver_event (state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
   int self = lp->gid;
 
-  // initialize the bit field
-  *(int *) bf = (int) 0;
+  s->forward_event_count++;
+  s->net_event_count++;
 
-  // update the current state
-  // however, save the old value in the 'reverse' message
-  SWAP(&(s->value), &(in_msg->contents));
-
-  // handle the message
   switch (in_msg->type) {
-    case HELLO :
+    case INCREMENT :
     {
-      s->rcvd_count_H++;
+      s->count += in_msg->value;
       break;
     }
-    case GOODBYE :
+    case DECREMENT :
     {
-      s->rcvd_count_G++;
+      s->count -= in_msg->value;
       break;
     }
     default :
-      printf("Unhandeled forward message type %d\n", in_msg->type);
+      tw_error(TW_LOC, "Unhandeled message type by receiver lp");
   }
-
-  tw_event *e = tw_event_new(self, 1, lp);
-  message *msg = tw_event_data(e);
-  //# randomly choose message type
-  double random = tw_rand_unif(lp->rng);
-  if (random < 0.5) {
-    msg->type = HELLO;
-  } else {
-    msg->type = GOODBYE;
-  }
-  msg->contents = tw_rand_unif(lp->rng);
-  msg->sender = self;
-  tw_event_send(e);
 }
 
 //Reverse Event Handler
 void receiver_event_reverse (state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
   int self = lp->gid;
 
-  // undo the state update using the value stored in the 'reverse' message
-  SWAP(&(s->value), &(in_msg->contents));
+  s->reverse_event_count++;
+  s->net_event_count--;
 
-  // handle the message
   switch (in_msg->type) {
-    case HELLO :
+    case INCREMENT :
     {
-      s->rcvd_count_H--;
+      s->count -= in_msg->value;
       break;
     }
-    case GOODBYE :
+    case DECREMENT :
     {
-      s->rcvd_count_G--;
+      s->count += in_msg->value;
       break;
     }
     default :
-      printf("Unhandeled reverse message type %d\n", in_msg->type);
+      tw_error(TW_LOC, "Unhandeled reverse message type by receiver LP");
   }
-
-  // don't forget to undo all rng calls
-  tw_rand_reverse_unif(lp->rng);
-  tw_rand_reverse_unif(lp->rng);
 }
 
 //report any final statistics for this LP
 void receiver_final (state *s, tw_lp *lp){
   int self = lp->gid;
-  printf("%d handled %d Hello and %d Goodbye messages\n", self, s->rcvd_count_H, s->rcvd_count_G);
+  printf("%d received %d forward and %d reverse messages: count is %d\n", self, s->forward_event_count, s->reverse_event_count, s->count);
 }
