868c6c18bb2314786ed98942cafed15f4b4d1512
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-09-03 14:32:55 -0400

    increment LP functionality

--- increment_driver.c
+++ increment_driver.c
@@ -5,14 +5,6 @@
 #include "ross.h"
 #include "suspend.h"
 
-//Helper Functions
-void SWAP (double *a, double *b) {
-  double tmp = *a;
-  *a = *b;
-  *b = tmp;
-}
-
-
 //Init function
 // - called once for each LP
 // ! LP can only send messages to itself during init !
@@ -20,16 +12,13 @@ void increment_init (state *s, tw_lp *lp) {
   int self = lp->gid;
 
   // init state data
-  s->rcvd_count_H = 0;
-  s->rcvd_count_G = 0;
-  s->value = -1;
+  s->increment_value = 1;
+  s->round_count = 0;
 
   // Init message to myself
   tw_event *e = tw_event_new(self, 1, lp);
   message *msg = tw_event_data(e);
-  msg->type = HELLO;
-  msg->contents = tw_rand_unif(lp->rng);
-  msg->sender = self;
+  msg->type = WAKEUP;
   tw_event_send(e);
 }
 
@@ -37,73 +26,29 @@ void increment_init (state *s, tw_lp *lp) {
 void increment_event (state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
   int self = lp->gid;
 
-  // initialize the bit field
-  *(int *) bf = (int) 0;
-
-  // update the current state
-  // however, save the old value in the 'reverse' message
-  SWAP(&(s->value), &(in_msg->contents));
-
-  // handle the message
-  switch (in_msg->type) {
-    case HELLO :
-    {
-      s->rcvd_count_H++;
-      break;
-    }
-    case GOODBYE :
-    {
-      s->rcvd_count_G++;
-      break;
-    }
-    default :
-      printf("Unhandeled forward message type %d\n", in_msg->type);
+  if (in_msg->type != WAKEUP) {
+    tw_error(TW_LOC, "Increment LP received wrong message type")
   }
 
-  tw_event *e = tw_event_new(self, 1, lp);
-  message *msg = tw_event_data(e);
-  //# randomly choose message type
-  double random = tw_rand_unif(lp->rng);
-  if (random < 0.5) {
-    msg->type = HELLO;
-  } else {
-    msg->type = GOODBYE;
+  s->round_count++;
+
+  int i;
+  for (i = 0; i < total_receivers; i++) {
+    tw_event *e = tw_event_new(i+2, 1, lp);
+    message *msg = tw_event_data(e);
+    msg->type = INCREMENT;
+    tw_event_send(e);
   }
-  msg->contents = tw_rand_unif(lp->rng);
-  msg->sender = self;
-  tw_event_send(e);
 }
 
 //Reverse Event Handler
 void increment_event_reverse (state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
-  int self = lp->gid;
-
-  // undo the state update using the value stored in the 'reverse' message
-  SWAP(&(s->value), &(in_msg->contents));
-
-  // handle the message
-  switch (in_msg->type) {
-    case HELLO :
-    {
-      s->rcvd_count_H--;
-      break;
-    }
-    case GOODBYE :
-    {
-      s->rcvd_count_G--;
-      break;
-    }
-    default :
-      printf("Unhandeled reverse message type %d\n", in_msg->type);
-  }
-
-  // don't forget to undo all rng calls
-  tw_rand_reverse_unif(lp->rng);
-  tw_rand_reverse_unif(lp->rng);
+  // there is nothing to do here
+  return;
 }
 
 //report any final statistics for this LP
 void increment_final (state *s, tw_lp *lp){
   int self = lp->gid;
-  printf("%d handled %d Hello and %d Goodbye messages\n", self, s->rcvd_count_H, s->rcvd_count_G);
+  printf("%d sent %d rounds of increment messages, each with value %d\n", self, s->round_count, s->increment_value);
 }
